43656ac14c91c2dcd5f9a747d06a47991c6851c7
rexleonardo <rexpleonardo@gmail.com>
2022-06-09 09:16:32 +0800

    Created Professional Level's Mobile Layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,16 @@
+import { MdOutlineMenu } from 'react-icons/md'
+const Logo = './assets/images/shared/Deloitte-Logo-Mobile.png'
+
 const Navbar: React.FC = () => {
     return (
-        <div>Navbar ito</div>
+        <div className="w-screen h-[70px] bg-black flex justify-between">
+            <div>
+                <img src={Logo} alt="Logo" className="w-[125px] ml-[20px] mt-[9.725px]" />
+            </div>
+            <div>
+                <MdOutlineMenu className="w-[48px] h-[48px] invert mr-[20px] mt-[11px]" />
+            </div>
+        </div>
     )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'Header-1': '#393939'
+      }
+    },
+  },
+  plugins: [],
+}
